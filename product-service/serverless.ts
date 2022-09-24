75231d4e53cf62d634a598e07d05da790b0ef66b
aliaksei.kamoska <zaliakseik@pttep.com>
2022-09-24 19:04:46 +0500

    feat: create and configure serverless template

---
+++ product-service/serverless.ts
@@ -0,0 +1,40 @@
+import type { AWS } from '@serverless/typescript';
+
+import { getProductList, getProductById } from '@functions/index';
+
+const serverlessConfiguration: AWS = {
+  service: 'productsservice',
+  frameworkVersion: '3',
+  plugins: ['serverless-esbuild'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    region: 'eu-west-1',
+    stage: 'dev',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+  },
+  // import the function via paths
+  functions: { getProductList, getProductById },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: true,
+      sourcemap: false,
+      exclude: ['aws-sdk'],
+      target: 'node16',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
