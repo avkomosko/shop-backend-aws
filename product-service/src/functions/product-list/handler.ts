5d5efa794a23d0fd82359c6a23c188c5d7930784
aliaksei.kamoska <zaliakseik@pttep.com>
2022-10-02 23:19:10 +0500

    feat: connect getProductList to DB

--- product-service/src/functions/product-list/handler.ts
+++ product-service/src/functions/product-list/handler.ts
@@ -1,19 +1,42 @@
+import { DynamoDB } from 'aws-sdk';
+
 import {
   formatJSOnErrorResponse,
   formatJSONResponse,
   ValidatedEventAPIGatewayProxyEvent,
 } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
-
 import { ProductListResponse } from '@functions/models/product-list.model';
-import { getProducts } from '@functions/utils/utils';
 import { HTTPCODE } from '@functions/constants';
+import { combineProductsWithStock } from '@functions/utils/utils';
+import {
+  ProductWithoutCount,
+  StockItem,
+} from '@functions/models/product.model';
+const { PRODUCTS_TABLE, STOCK_TABLE } = process.env;
+
+const dynamo = new DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 
 const getProductList: ValidatedEventAPIGatewayProxyEvent<
   ProductListResponse
 > = async () => {
   try {
-    const products = await getProducts();
+    const { Items: productsList } = await dynamo
+      .scan({
+        TableName: PRODUCTS_TABLE,
+      })
+      .promise();
+
+    const { Items: stock } = await dynamo
+      .scan({
+        TableName: STOCK_TABLE,
+      })
+      .promise();
+
+    const products = combineProductsWithStock(
+      productsList as Array<ProductWithoutCount>,
+      stock as Array<StockItem>
+    );
     return formatJSONResponse({ products });
   } catch (error) {
     return formatJSOnErrorResponse(HTTPCODE.SERVER_ERROR, error);
