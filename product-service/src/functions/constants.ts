032e84e2b8343e8901380a952e4ee80f56af1001
aliaksei.kamoska <zaliakseik@pttep.com>
2022-10-02 22:21:59 +0500

    feat: add new constants

--- product-service/src/functions/constants.ts
+++ product-service/src/functions/constants.ts
@@ -1,7 +1,15 @@
 export const PRODUCT_NOT_FOUND_MESSAGE = 'Product not found';
 
+export const DATA_CREATED = 'Data created';
+
 export const enum HTTPCODE {
   OK = 200,
+  CREATED = 201,
   NOT_FOUND = 404,
   SERVER_ERROR = 500,
+}
+
+export const enum Tables {
+  PRODUCTS = 'Products',
+  STOCK = 'Stock',
 }
