86a8feb02440a760f55cab475b6038cc1f094ffe
aliaksei.kamoska <zaliakseik@pttep.com>
2022-10-03 01:59:51 +0500

    feat: add config to serverless

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -2,7 +2,8 @@ import { swaggerConfig } from './swagger.config';
 import { esBuildConfiguration } from './esBuild.config';
 import type { AWS } from '@serverless/typescript';
 
-import { getProductList, getProductById } from '@functions/index';
+import { getProductList, getProductById, uploadToDB, createProduct } from '@functions/index';
+import { Tables } from '@functions/constants';
 
 const serverlessConfiguration: AWS = {
   service: 'productsservice',
@@ -21,14 +22,76 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
+    iamRoleStatements:[
+      {
+        Effect: 'Allow',
+        Action: [
+          'dynamodb:Query',
+          'dynamodb:Scan',
+          'dynamodb:GetItem',
+          'dynamodb:PutItem',
+          'dynamodb:UpdateItem',
+          'dynamodb:DeleteItem'
+        ],
+        Resource: "arn:aws:dynamodb:eu-west-1:559851237825:table/*"
+      }
+    ]
   },
   // import the function via paths
-  functions: { getProductList, getProductById },
+  functions: { getProductList, getProductById, uploadToDB, createProduct },
   package: { individually: true },
   custom: {
     esbuild: esBuildConfiguration,
     autoswagger: swaggerConfig,
   },
+  resources: {
+    Resources: {
+      [Tables.PRODUCTS]: {
+        Type: 'AWS::DynamoDB::Table',
+        Properties: {
+          TableName: Tables.PRODUCTS,
+          AttributeDefinitions: [
+            {
+              AttributeName: 'id',
+              AttributeType: 'S'
+            },
+          ],
+           KeySchema: [
+            {
+              AttributeName: 'id',
+              KeyType: 'HASH'
+            },
+          ],
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1
+          },
+        }
+      },
+      [Tables.STOCK]: {
+        Type: 'AWS::DynamoDB::Table',
+        Properties: {
+          TableName: Tables.STOCK,
+          AttributeDefinitions: [
+            {
+              AttributeName: 'product_id',
+              AttributeType: 'S'
+            },
+          ],
+           KeySchema: [
+            {
+              AttributeName: 'product_id',
+              KeyType: 'HASH'
+            },
+          ],
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1
+          },
+        }
+      },
+    }
+  }
 };
 
 module.exports = serverlessConfiguration;
