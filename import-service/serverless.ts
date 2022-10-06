d0e2287091568f8d92b233358ab43d3c38f7b086
aliaksei.kamoska <zaliakseik@pttep.com>
2022-10-06 22:23:05 +0500

    feat: add import service template

---
+++ import-service/serverless.ts
@@ -0,0 +1,32 @@
+import { esBuildConfiguration } from './../product-service/esBuild.config';
+import type { AWS } from '@serverless/typescript';
+
+import hello from '@functions/hello';
+
+const serverlessConfiguration: AWS = {
+  service: 'import-service',
+  frameworkVersion: '3',
+  plugins: ['serverless-esbuild'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    region: 'eu-west-1',
+    stage: 'dev',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+  },
+  // import the function via paths
+  functions: { hello },
+  package: { individually: true },
+  custom: {
+    esbuild: esBuildConfiguration,
+  },
+};
+
+module.exports = serverlessConfiguration;
