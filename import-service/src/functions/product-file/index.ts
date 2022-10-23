7e1518ee85a09b98c637229d997d36b5a9581934
aliaksei.kamoska <aliaksei.kamoska@ihsmarkit.com>
2022-10-23 16:00:24 +0500

    feat: add lambda authorization to import service

--- import-service/src/functions/product-file/index.ts
+++ import-service/src/functions/product-file/index.ts
@@ -1,4 +1,8 @@
 import { handlerPath } from '@libs/handler-resolver';
+import { env } from 'process';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
@@ -8,6 +12,12 @@ export default {
         method: 'get',
         path: '/import',
         cors: true,
+        authorizer: {
+          arn: env.AUTHORIZER_ARN,
+          identitySource: 'method.request.header.authorization',
+          resultTtlInSeconds: 0,
+          type: 'token',
+        },
       },
     },
   ],
